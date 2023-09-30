3337df1b6d6814310b67e79dd3f596a702a0a9d7
Hemasai1432 <117273575+Hemasai1432@users.noreply.github.com>
2023-09-30 11:06:11 +0530

    Update diabetes_prediction_webapp.py

--- diabetes_prediction_webapp.py
+++ diabetes_prediction_webapp.py
@@ -5,7 +5,7 @@ import pickle
 import streamlit as st
 
 # loading the saved model
-loaded_model=pickle.load(open('trained_model.sav','rb'))
+loaded_model=pickle.load(open('new_trained_model.sav','rb'))
 
 def diabetes_prediction(input_data):
     
